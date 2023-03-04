96bf2e73b89aa02179bdce6ff761b69929fad95d
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2023-03-04 23:31:03 +0100

    HyperCurrencies + Configs

---
+++ src/main/java/me/sv3ks/hypercurrencies/HyperCurrencies.java
@@ -0,0 +1,36 @@
+package me.sv3ks.hypercurrencies;
+
+import me.sv3ks.hypercurrencies.utils.Config;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HyperCurrencies extends JavaPlugin {
+
+    private static Plugin plugin;
+    private static Config currencyConfig;
+    private static Config dataConfig;
+
+    @Override
+    public void onEnable() {
+
+        plugin = this;
+        currencyConfig = new Config("currencies.yml");
+        dataConfig = new Config("data.yml");
+
+        currencyConfig.createConfig();
+        dataConfig.createConfig();
+
+        this.getLogger().info("Hyper was enabled");
+
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Hyper was disabled");
+    }
+
+    public static Plugin getPlugin() { return plugin; }
+    public static Config getCurrencyConfig() { return currencyConfig; }
+    public static Config getDataConfig() { return dataConfig; }
+
+}
---
+++ src/main/java/me/sv3ks/hypercurrencies/utils/Config.java
@@ -0,0 +1,49 @@
+package me.sv3ks.hypercurrencies.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+import static me.sv3ks.hypercurrencies.HyperCurrencies.getPlugin;
+
+public class Config {
+
+    private final String fileName;
+    private File configFile;
+    private FileConfiguration config;
+
+    public Config(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(configFile);
+            //YamlConfiguration.loadConfiguration(dataConfigFile);
+        } catch (IOException e) {
+            Bukkit.getServer().getLogger().info("Error while saving data config:");
+            Bukkit.getServer().getLogger().info(e.toString());
+        }
+    }
+
+    public void createConfig() {
+        configFile = new File(getPlugin().getDataFolder(), fileName);
+        if (!configFile.exists()) {
+            if (!configFile.getParentFile().mkdirs()) getPlugin().getLogger().severe(ChatColor.DARK_RED + "Failed to create necessary directories.");
+            // dataConfigFile.getParentFile().mkdirs();
+            getPlugin().saveResource(fileName, false);
+        }
+
+        config = new YamlConfiguration();
+
+        YamlConfiguration.loadConfiguration(configFile);
+    }
+}
