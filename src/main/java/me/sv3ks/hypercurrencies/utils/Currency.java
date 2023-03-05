757f117ed35daaf8b56d256f441afebd5ccb7e9d
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2023-03-05 16:54:29 +0100

    Currency utilities

---
+++ src/main/java/me/sv3ks/hypercurrencies/utils/Currency.java
@@ -0,0 +1,49 @@
+package me.sv3ks.hypercurrencies.utils;
+
+import java.util.UUID;
+
+import static me.sv3ks.hypercurrencies.HyperCurrencies.getDataConfig;
+
+public class Currency {
+    /*
+    private final String name;
+
+    public Currency(String name) {
+        this.name = name;
+    }
+
+    public long getBalance(UUID player) {
+        return getDataConfig().getConfig().getLong(name+"."+player);
+    }
+
+    public void addBalance(UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, getBalance(player)+amount);
+    }
+
+    public void removeBalance(UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, getBalance(player)-amount);
+    }
+
+    public void setBalance(UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, amount);
+    }
+
+    public String getName() { return name; }
+     */
+
+    public static long getBalance(String name, UUID player) {
+        return getDataConfig().getConfig().getLong(name+"."+player);
+    }
+
+    public static void addBalance(String name, UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, getBalance(name, player)+amount);
+    }
+
+    public static void removeBalance(String name, UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, getBalance(name, player)-amount);
+    }
+
+    public static void setBalance(String name, UUID player, long amount) {
+        getDataConfig().getConfig().set(name+"."+player, amount);
+    }
+}
