7e8e5ac119d556435bd21aee1396c0500f6b9bcf
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2023-03-08 17:48:17 +0100

    /currency remove & get

--- src/main/java/me/sv3ks/hypercurrencies/commands/CurrencyCommand.java
+++ src/main/java/me/sv3ks/hypercurrencies/commands/CurrencyCommand.java
@@ -4,10 +4,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import static me.sv3ks.hypercurrencies.HyperCurrencies.getPlugin;
-import static me.sv3ks.hypercurrencies.utils.Currency.addBalance;
-import static me.sv3ks.hypercurrencies.utils.Currency.currencyExists;
+import static me.sv3ks.hypercurrencies.utils.Currency.*;
 import static me.sv3ks.hypercurrencies.utils.Utils.msgWrap;
 import static me.sv3ks.hypercurrencies.utils.Utils.wrap;
 import static org.bukkit.Bukkit.getPlayer;
@@ -16,8 +16,8 @@ public class CurrencyCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (args[0].equalsIgnoreCase("add")||args[0].equalsIgnoreCase("give")) {
-            if (args[1]==null&&args[2]==null&&args[3]==null) {
+        if (args[0].equalsIgnoreCase("add")||args[0].equalsIgnoreCase("give")||args[0].equalsIgnoreCase("remove")||args[0].equalsIgnoreCase("revoke")) {
+            if (args[3]==null) {
                 sender.sendMessage(msgWrap("&cInvalid amounts of arguments."));
                 return false;
             }
@@ -33,10 +33,42 @@ public class CurrencyCommand implements CommandExecutor {
             }
 
             try {
-                addBalance(args[3],getPlayer(args[1]).getUniqueId(),Long.parseLong(args[2]));
+                Long.parseLong(args[2]);
             } catch (NumberFormatException e) {
                 sender.sendMessage(msgWrap("&cInvalid amount."));
+                return false;
             }
+
+            if (args[0].equalsIgnoreCase("add")||args[0].equalsIgnoreCase("give")) {
+                addBalance(args[3], getPlayer(args[1]).getUniqueId(), Long.parseLong(args[2]));
+                sender.sendMessage(msgWrap("&aGave "+args[2]+" "+args[3]+" to "+sender.getName()));
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("remove")||args[0].equalsIgnoreCase("revoke")) {
+                addBalance(args[3], getPlayer(args[1]).getUniqueId(), Long.parseLong(args[2]));
+                sender.sendMessage(msgWrap("&aRemoved "+args[2]+" "+args[3]+" from "+sender.getName()));
+                return true;
+            }
+        }
+
+        if (args[0].equalsIgnoreCase("info")||args[0].equalsIgnoreCase("get")) {
+            if (args[2]==null) {
+                sender.sendMessage(msgWrap("&cInvalid amounts of arguments."));
+                return false;
+            }
+
+            if (getPlayer(args[1])==null) {
+                sender.sendMessage(msgWrap("&cInvalid player."));
+                return false;
+            }
+
+            if (!currencyExists(args[2])) {
+                sender.sendMessage(msgWrap("&cUnknown currency."));
+                return false;
+            }
+
+            sender.sendMessage(msgWrap("&a"+args[1]+" has "+getBalance(args[2],getPlayer(args[1]).getUniqueId())+" "+args[2]+"."));
         }
 
         if (args[0].equalsIgnoreCase("help")) {
@@ -46,6 +78,7 @@ public class CurrencyCommand implements CommandExecutor {
             sender.sendMessage(wrap("&d/currency <add|give> <player> <amount> <currency> &8- &5Gives currency to a player."));
             sender.sendMessage(wrap("&d/currency <remove|revoke> <player> <amount> <currency> &8- &5Removes currency from a player."));
             sender.sendMessage(wrap("&d/currency <info|get> <player> <currency> &8- &5Gets a player's currency."));
+            sender.sendMessage(wrap("&d/currency <create> <name> &8- &5Creates a new currency."));
 
             return true;
         }
